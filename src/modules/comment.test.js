15fecb17dc2a926d773736f839ca4b3adb05a624
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-03-17 12:46:52 +0300

    Add item count test

--- src/modules/comment.test.js
+++ src/modules/comment.test.js
@@ -2,31 +2,35 @@
  * @jest-environment jsdom
  */
 
-import { getComment } from './API';
+import { getComment } from './API.js';
 
+const getMovieCount = (items) => {
+  const result = items.length;
+  return result;
+};
 const appendComment = (data) => {
-    const commentList = document.createElement('li');
-    commentList.innerHTML = `${data.comment}`;
-    return commentList;
-  };
+  const commentList = document.createElement('li');
+  commentList.innerHTML = `${data.comment}`;
+  return commentList;
+};
 
 global.fetch = jest.fn(() => {
-    const testData = [
-      {
-        item_id: 'item1',
-        username: 'Basim',
-        comment: 'Test1',
-      },
-      {
-        item_id: 'item1',
-        username: 'Dheeraj',
-        comment: 'Test2',
-      },
-    ];
-    return Promise.resolve({
-      json: () => Promise.resolve(testData),
-    });
+  const testData = [
+    {
+      item_id: 'item1',
+      username: 'Basim',
+      comment: 'Test1',
+    },
+    {
+      item_id: 'item1',
+      username: 'Dheeraj',
+      comment: 'Test2',
+    },
+  ];
+  return Promise.resolve({
+    json: () => Promise.resolve(testData),
   });
+});
 
 beforeEach(() => {
   fetch.mockClear();
@@ -38,7 +42,7 @@ describe('Comments counter test', () => {
     + '  <ul id="comments"></ul>'
     + '</div>';
     const commentsList = document.getElementById('comments');
-    const nComments = commentsList.childNodes.length
+    const nComments = commentsList.childNodes.length;
     expect(nComments).toBe(0);
   });
 
@@ -46,12 +50,20 @@ describe('Comments counter test', () => {
     document.body.innerHTML = '<div>'
    + '  <ul id="comments"></ul>'
    + '</div>';
-   const commentsList = document.getElementById('comments')
-   const res = await getComment('item1')
+    const commentsList = document.getElementById('comments');
+    const res = await getComment('item1');
     res.forEach((data) => {
-        commentsList.appendChild(appendComment(data));
+      commentsList.appendChild(appendComment(data));
     });
-    const nComments = commentsList.childNodes.length
-      expect(nComments).toBe(2);
-  })
-})
+    const nComments = commentsList.childNodes.length;
+    expect(nComments).toBe(2);
+  });
+});
+
+describe('Movies counter testing', () => {
+  test('Number of movies', () => {
+    const movieList = [{ name: 'Hero' }, { name: 'Mission Impossible' }];
+    const movieNum = getMovieCount(movieList);
+    expect(movieNum).toBe(2);
+  });
+});
--- src/modules/likes.test.js
+++ src/modules/likes.test.js
@@ -2,28 +2,28 @@
  * @jest-environment jsdom
  */
 
-import { getLikes } from "./API"
+import { getLikes } from './API.js';
 
 global.fetch = jest.fn(() => Promise.resolve({
-    json: () => Promise.resolve(
-      [
-        {
-          likes: {
-            item_id: 'item1',
-          },
+  json: () => Promise.resolve(
+    [
+      {
+        likes: {
+          item_id: 'item1',
         },
-        {
-          likes: {
-            item_id: 'item1',
-          },
+      },
+      {
+        likes: {
+          item_id: 'item1',
         },
-      ],
-    ),
-  }));
-  describe('likes counter', () => {
-    it('test likes counter', async () => {
-      const res = await getLikes();
-  
-      expect(res.length).toBe(2);
-    });
+      },
+    ],
+  ),
+}));
+describe('likes counter', () => {
+  it('test likes counter', async () => {
+    const res = await getLikes();
+
+    expect(res.length).toBe(2);
   });
+});
